bba5c495e81ef3ddac084a9c3de0968790877035
Adam Green <adam.green@118group.co.uk>
2019-05-09 20:48:08 +0100

    updated to use new API

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var Service, Characteristic;
-const { getSessionCookie, getStatus, setStatus } = require('yalealarmsystem');
+//const { getSessionCookie, getStatus, setStatus } = require('yalealarmsystem');
+var fetch = require('node-fetch');
+
+var _HOST = "https://mob.yalehomesystem.co.uk/yapi";
+var yaleAuthToken = 'VnVWWDZYVjlXSUNzVHJhcUVpdVNCUHBwZ3ZPakxUeXNsRU1LUHBjdTpkd3RPbE15WEtENUJ5ZW1GWHV0am55eGhrc0U3V0ZFY2p0dFcyOXRaSWNuWHlSWHFsWVBEZ1BSZE1xczF4R3VwVTlxa1o4UE5ubGlQanY5Z2hBZFFtMHpsM0h4V3dlS0ZBcGZzakpMcW1GMm1HR1lXRlpad01MRkw3MGR0bmNndQ==';
+var refresh_token = '';
+var access_token = '';
 
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
@@ -28,12 +34,23 @@ function YaleAlarm(log, config) {
 YaleAlarm.prototype.getState = function(callback) {
     this.log("Getting current state...");
 
-    getSessionCookie(
-        this.config.username, 
-        this.config.password
-    ).then(getStatus).then((response) => {
-        callback(null, response.message[0].mode === 'arm');
-    }).catch(console.log);
+
+    //make a request to get auth token
+    authorize()
+        .then(response => response.json())//load it as json
+        //save the token to global variable
+        .then(data => {
+            access_token = data.access_token;
+            refresh_token = data.refresh_token;
+            //fetch current url using new token.
+            return fetch(_HOST + "/api/panel/mode/", { method: 'GET', headers: getAuthHeaders() });
+        })
+        .then(response =>
+            response.json()
+        ) // Load the response as json
+        .then(res => {
+            callback(null, res.data[0].mode === 'arm');         
+        }).catch(console.log);    
 }
 
 YaleAlarm.prototype.setState = function(state, callback) {
@@ -41,24 +58,69 @@ YaleAlarm.prototype.setState = function(state, callback) {
 
     console.log(`Set Alarm state to ${alarmState}`);
 
-    getSessionCookie(
-        this.config.username, 
-        this.config.password
-    ).then((sessionCookie) => {
-        setStatus(sessionCookie, alarmState).then(resp => resp.json()).then((response) => {
+    //make a request to get auth token
+    authorize()
+        .then(response => response.json())//load it as json
+        //save the token to global variable
+        .then(data => {
+            access_token = data.access_token;
+            refresh_token = data.refresh_token;
+            //fetch current url using new token.
+            return fetch(_HOST + "/api/panel/mode/",
+                {
+                    method: 'POST',
+                    body: `area=1&mode=${alarmState}`,
+                    headers: {
+                        "Authorization": "Bearer " + access_token,
+                        'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+                        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                    }
+                }
+            );
+        })
+        .then(response =>
+            response.json()
+        ) // Load the response as json
+        .then(res => {
+            
             var currentState = (state == Characteristic.LockTargetState.SECURED) ?
-            Characteristic.LockCurrentState.SECURED : Characteristic.LockCurrentState.UNSECURED;
-        
+                Characteristic.LockCurrentState.SECURED : Characteristic.LockCurrentState.UNSECURED;
+
             this.service.setCharacteristic(Characteristic.LockCurrentState, currentState);
-        
             callback(null); // success
+
         }).catch((response) => {
             console.log('catch')
             console.log(response)
-        });
-    });
+        });    
 }
 
 YaleAlarm.prototype.getServices = function() {
     return [this.service];
+}
+
+
+function getAuthHeaders() {
+    return {
+        "Authorization": "Bearer " + access_token
+    }
+}
+
+function authorize() {
+    var payload = `grant_type=refresh_token&refresh_token=${refresh_token}`;
+    if (refresh_token === "") {
+        payload = `grant_type=password&username=${encodeURIComponent(this.config.username)}&password=${encodeURIComponent(this.config.password)}`;
+    };
+    return fetch(_HOST + "/o/token/",
+        {
+            method: 'POST',
+            body: payload,
+            headers: {
+                "Authorization": "Basic " + yaleAuthToken,
+                'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            }
+        }
+    );
+
 }
